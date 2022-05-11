9225a8a5c5b6a9065aa62b663f5d94df762b4217
Premansh Dewangan <72143644+premanshdewangan@users.noreply.github.com>
2022-05-11 08:02:10 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,6 +1,40 @@
 #include "stats.h"
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    int a = 10;
+struct Stats{
+    double max;
+    double min;
+    double average;
+};
+
+typedef struct Stats Stats;
+
+Stats Statistics::ComputeStatistics(const std::vector<double> &v ) {
     //Implement statistics here
+    Stats obj;
+    
+    obj.min = 0;
+    obj.max = 0;
+    obj.average = 0;
+    
+    double sum = 0;
+    int len = v.size();
+    
+    for(auto itr : v)
+    {
+        if(itr < obj.min)
+            obj.min = itr;
+        
+        if(itr > obj.max)
+            obj.max = itr;
+        
+        sum += itr;
+        
+    }
+    
+    
+    obj.average = sum/len ;
+    
+    return obj;
+    
+    
 }
