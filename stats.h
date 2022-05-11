e3fdd4ae1025eaeab4e26f05863d4fcc2122cf0c
Premansh Dewangan <72143644+premanshdewangan@users.noreply.github.com>
2022-05-11 07:45:46 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,13 +1,5 @@
 #include <vector>
 
-struct Stats{
-    int max;
-    int min;
-    int average;
-};
-
-typedef struct Stats Stats;
-
 namespace Statistics {
     Stats ComputeStatistics(const std::vector<___>& );
 }
