8ad4998d470c276924b703405b511773b295ba5b
Premansh Dewangan <72143644+premanshdewangan@users.noreply.github.com>
2022-05-11 09:34:42 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -12,3 +12,32 @@ namespace Statistics {
     
     Stats ComputeStatistics(const std::vector<double>& );
 }
+
+class IAlerter{
+    public:
+        virtual void alertMessage(string) = 0;
+};
+
+class EmailAlert : public IAlerter{
+    public:
+        bool emailSent;
+        void alertMessage(string);
+};
+
+class LEDAlert : public IAlerter{
+    public:
+        bool ledGlows;
+        void alertMessage(string);
+};
+
+
+class StatsAlerter{
+    private:
+        float threshold;
+        std::vector<IAlerter*> alerters;
+            
+    public:
+        StatsAlerter(float, std::vector<IAlerter*> &);
+    
+        void checkAndAlert(vector<float> &);
+};
